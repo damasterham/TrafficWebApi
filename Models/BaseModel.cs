ac9e2dd59a41347e4e97188d8aa1f63b5e35a76e
damasterham <cmeisterham@gmail.com>
2020-02-10 01:06:37 +0100

    Adds models: BaseModel and LineInfo

---
+++ Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafikWebAPI.Models
+{
+    // A base model that will hold properties that would be reused between all models
+    // So for example if it was required to have time stamps for creation and updates
+    // for all our models we could just set it here
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Models/LineInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafikWebAPI.Models
+{
+    public class LineInfo : BaseModel
+    {
+        // Would probably define lines somewhere as possibly and enum or list of strings
+        // For validation as well as possible efficiency
+        public string Line { get; set; }
+        // There should probably more validation and such for DateTime since
+        // working with time is notoriously difficuly
+        // But keeping it simple for this example
+        public DateTime Time { get; set; }
+
+        public string Message { get; set; }
+    }
+}
