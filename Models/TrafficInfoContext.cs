4e811f82b43f7e4a3560e265e5c8a93d043c41ec
damasterham <cmeisterham@gmail.com>
2020-02-10 01:14:12 +0100

    Adds TrafficInfoContext (DbContext) and registers it in Startup
    TrafficInfoContext registers LineInfo as a data object and creates
    seed data
    
    It is then registered as InMemory and ensures that
    data will be created for seed data

---
+++ Models/TrafficInfoContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafikWebAPI.Models
+{
+    // Our data context where we can define the collections od data we want to persist,
+    // their references and the like
+    public class TrafficInfoContext : DbContext
+    {
+        public DbSet<LineInfo> LineInfoEntries { get; set; }
+
+        public TrafficInfoContext(DbContextOptions<TrafficInfoContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Initialze some seed data
+            LineInfo[] seedData = new LineInfo[20];
+
+            for (int i = 0; i < seedData.Length; i++)
+            {
+                seedData[i] = new LineInfo
+                {
+                    Id = i + 1,
+                    Line = string.Format("{0}A", ((i+1) / 2) % 2 + 1 ),
+                    Time = new DateTime(2020, 2, 7, 10, 26 + i, 56),
+                    Message = string.Format("Message {0}", i)
+                };
+            }
+
+            modelBuilder.Entity<LineInfo>().HasData(seedData);
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,9 +10,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TrafikWebAPI.Models;
 
 namespace TrafikWebAPI
 {
+
+    // So at the moment for this case, I cannot really think of any aplicable design patterns
+    // other than the ones inhentily used with the MVC framewok, instead of implementing our own singleton for example
+    // we could make use of just registering a transient service
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -22,13 +28,21 @@ namespace TrafikWebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // So for this case i'll just be utilizing EF Core, since ORM's are nice and simple
+            // We could of course delve into using different types of DBs within the SQL NoSQL realm
+            // either by still utlizing ef core for whatever it can support, or make custom data access possible
+            // Either would most likely beneift from an added layer of abstraction making interfaces around 
+            // the different data access to allow to for easier swapping of data access or 'services'
+
+            // Adding an in memory database for this case, which from what can suss utilized sqlite
+            // We are registering here with the ServiceProvider so we can access it throught this project
+            services.AddDbContext<TrafficInfoContext>(options => options.UseInMemoryDatabase("TrafficDb"));
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,6 +60,14 @@ namespace TrafikWebAPI
             {
                 endpoints.MapControllers();
             });
+
+            // To ensure Seed Data is created for the LineInfoEntries
+            var context = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope()
+                .ServiceProvider
+                .GetService<TrafficInfoContext>();
+            context.Database.EnsureCreated();
         }
     }
 }
